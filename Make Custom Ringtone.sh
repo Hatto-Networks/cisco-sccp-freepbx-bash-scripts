3181a17e1916d23cb2ab8c00d54618194d976e45
carriethekid <62395331+akidinatophat@users.noreply.github.com>
2023-03-14 15:37:55 -0400

    Update Make Custom Ringtone.sh

--- Make Custom Ringtone.sh
+++ Make Custom Ringtone.sh
@@ -3,9 +3,9 @@
 # sox $inputfile -t raw -r 8000 -e mu-law -c 1 $outname.raw
 
 echo -e "Welcome to the Cisco Ringtone maker. The file you want to convert must be a WAV file and under 15 seconds."
-echo -e "\n\nWhere is your input file at?\n\n1. Local file\n2. On a web server\n\nType an option..."
+echo -e "\n\nWhere is your input file at?\n\nA. Local file\nB. On a web server\n\nType an option..."
 read $opt
-if [ "$opt" == "1" ]; then
+if [ "$opt" == "A" ]; then
     echo -e "You have selected Local file. Enter the file path. If it's in the same directory you're in, just type the filename."
     read inputfile
     echo -e "Type the name of the ringtone file."
@@ -13,7 +13,7 @@ if [ "$opt" == "1" ]; then
     sox $inputfile -t raw -r 8000 -e mu-law -c 1 $outname.raw
     echo -e "Your ringtone file has been saved to $outname.raw"
     exit
-elif [ "$opt" == "2" ]; then
+elif [ "$opt" == "B" ]; then
     echo -e "You have selected Web server. Type the full address of the file name. Example: https://hatto.dev/res/sound.wav"
     read weburl
     wget -q $weburl
